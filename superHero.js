d78a4829497ff45a8afbafc51af720f1a64c5ddf
vmarques339 <73534183+vmarques339@users.noreply.github.com>
2021-02-05 19:55:01 +0000

    Add files via upload

---
+++ superHero.js
@@ -0,0 +1,22 @@
+class superHero{
+    constructor(x,y,width,height,angle){
+        var options={
+            density:1,
+            frictionAir:0.005      
+        }
+        this.image = loadImage("Superhero-01.png");
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+        var angle=this.body.angle;
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(angle);
+        ellipse(0,0,this.width,this.height);
+        pop();
+    }
+
+}
